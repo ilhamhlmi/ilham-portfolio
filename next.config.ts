5e10061607a1325cf253934acf1c72362e8e3ab4
ilhamhlmi <ilhamhilmi206@gmail.com>
2025-08-30 23:28:17 +0700

    last

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",  // ⬅️ WAJIB supaya hasil build jadi static export
   basePath: "/ilham-portfolio",   // ⬅️ harus sama persis dengan nama repo
-  assetPrefix: "/ilham-portfolio/", 
+  assetPrefix: "/ilham-portfolio/",
+  images: {
+    unoptimized: true,   // ⬅️ WAJIB untuk GitHub Pages
+  },
 };
 
 export default nextConfig;
